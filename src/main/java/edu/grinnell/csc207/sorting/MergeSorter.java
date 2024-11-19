1d7d1351900a4197533cb29ec2ae1bba96f9ffc3
Luis Lopez <ruk@Luiss-MacBook-Air.local>
2024-11-18 23:03:55 -0600

    Added Hlper merge

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
  * Something that sorts using merge sort.
  *
  * @param <T>
- *   The types of values that are sorted.
+ *            The types of values that are sorted.
  *
  * @author Samuel A. Rebelsky
  * @author Luis Lopez
@@ -31,8 +31,8 @@ public class MergeSorter<T> implements Sorter<T> {
    * Create a sorter using a particular comparator.
    *
    * @param comparator
-   *   The order in which elements in the array should be ordered
-   *   after sorting.
+   *                   The order in which elements in the array should be ordered
+   *                   after sorting.
    */
   public MergeSorter(Comparator<? super T> comparator) {
     this.order = comparator;
@@ -43,32 +43,18 @@ public class MergeSorter<T> implements Sorter<T> {
   // +---------+
 
   /**
-   * Sort an array in place using merge sort.
+   * Merges two sorted subarrays of an original array to sort original.
    *
    * @param values
-   *   an array to sort.
-   *
+   *               an array to be sorted
+   * @param left
+   *               sorted left subarray of values
+   * @param right
+   *               sorted right subarray of values
    * @post
-   *   The array has been sorted according to some order (often
-   *   one given to the constructor).
-   * @post
-   *   For all i, 0 &lt; i &lt; values.length,
-   *     order.compare(values[i-1], values[i]) &lt;= 0
+   *       merge two subarrays of values back into values
    */
-  @Override
-  public void sort(T[] values) {
-    int rightArrStart = values.length / 2;
-
-    T[] left = Arrays.copyOfRange(values, 0, rightArrStart);
-    T[] right = Arrays.copyOfRange(values, rightArrStart, values.length);
-
-    if (left.length > 1) {
-      sort(left);
-    } // if subarray is bigger than one element, sort
-    if (right.length > 1) {
-      sort(right);
-    } // if subarray is bigger than one element, sort
-
+  public void merge(T[] values, T[] left, T[] right) {
     int leftInd = 0;
     int rightInd = 0;
 
@@ -86,5 +72,35 @@ public class MergeSorter<T> implements Sorter<T> {
       } // checks if element in array is less than to copy to original array
         // or if only one array is left copies from there
     } // loop over entire original array
+  } // merge(T[], T[], T[])
+
+  /**
+   * Sort an array in place using merge sort.
+   *
+   * @param values
+   *               an array to sort.
+   *
+   * @post
+   *       The array has been sorted according to some order (often
+   *       one given to the constructor).
+   * @post
+   *       For all i, 0 &lt; i &lt; values.length,
+   *       order.compare(values[i-1], values[i]) &lt;= 0
+   */
+  @Override
+  public void sort(T[] values) {
+    int rightArrStart = values.length / 2;
+
+    T[] left = Arrays.copyOfRange(values, 0, rightArrStart);
+    T[] right = Arrays.copyOfRange(values, rightArrStart, values.length);
+
+    if (left.length > 1) {
+      sort(left);
+    } // if subarray is bigger than one element, sort
+    if (right.length > 1) {
+      sort(right);
+    } // if subarray is bigger than one element, sort
+
+    merge(values, left, right);
   } // sort(T[])
 } // class MergeSorter
